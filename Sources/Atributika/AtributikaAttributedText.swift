fa42191c87e8b43b43610cb98d5827b7e8ac37ff
Shaw <cuzval@gmail.com>
2023-12-27 16:37:51 +0800

    Update AtributikaAttributedText

--- Sources/Atributika/AtributikaAttributedText.swift
+++ Sources/Atributika/AtributikaAttributedText.swift
@@ -3,15 +3,23 @@ import SwiftUI
 
 public struct AtributikaAttributedText: UIViewRepresentable {
   let attrsBuilder: AttributedStringBuilder
+  let alignment: NSTextAlignment
   let onClick: ((Any) -> Void)?
   
-  public init(attrsBuilder: AttributedStringBuilder, onClick: ((Any) -> Void)? = nil) {
+  public init(
+    attrsBuilder: AttributedStringBuilder,
+    alignment: NSTextAlignment = .justified,
+    onClick: ((Any) -> Void)? = nil
+  ) {
     self.attrsBuilder = attrsBuilder
+    self.alignment = alignment
     self.onClick = onClick
   }
   
   public func makeUIView(context: Context) -> AttributedLabel {
     let it = AttributedLabel()
+    it.numberOfLines = 0
+    it.translatesAutoresizingMaskIntoConstraints = false
     it.onLinkTouchUpInside = { _, value in
       onClick?(value)
     }
@@ -20,6 +28,7 @@ public struct AtributikaAttributedText: UIViewRepresentable {
   
   public func updateUIView(_ uiView: UIViewType, context: Context) {
     uiView.attributedTextBuilder(attrsBuilder)
+    uiView.textAlignment = alignment
   }
 }
 
